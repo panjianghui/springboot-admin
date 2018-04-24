d8feabc057265d373d1b5268f089f0e9582bf4ea
panjianghui <546540274@qq.com>
2018-04-25 00:15:11 +0800

    提交

---
+++ file/src/main/java/com/pjh/v1/FileUtil.java
@@ -0,0 +1,19 @@
+package com.pjh.v1;
+
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileOutputStream;
+@Component
+public class FileUtil {
+    public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception {
+        File targetFile = new File(filePath);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        FileOutputStream out = new FileOutputStream(filePath+fileName);
+        out.write(file);
+        out.flush();
+        out.close();
+    }
+}
---
+++ file/src/main/java/com/pjh/v1/controller/UploadImgController.java
@@ -0,0 +1,38 @@
+package com.pjh.v1.controller;
+
+import com.pjh.v1.FileUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+@Controller
+public class UploadImgController {
+    //跳转到上传文件的页面
+    @RequestMapping(value="/gouploadimg", method = RequestMethod.GET)
+    public String goUploadImg() {
+        //跳转到 templates 目录下的 uploadimg.html
+        return "uploadimg";
+    }
+
+    //处理文件上传
+    @RequestMapping(value="/testuploadimg", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadImg(@RequestParam("file") MultipartFile file,
+                     HttpServletRequest request) {
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+        try {
+            System.out.println(filePath + fileName);
+            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //返回json
+        return "uploadimg success";
+    }
+}
