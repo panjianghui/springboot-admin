25cc75f28a23ed67eeeec1de62386d3199a49fde
panjianghui <546540274@qq.com>
2018-04-25 23:41:14 +0800

    提交

--- portal/src/main/java/com/pjh/v1/PortalApplication.java
+++ portal/src/main/java/com/pjh/v1/PortalApplication.java
@@ -1,12 +1,22 @@
 package com.pjh.v1;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author 辉
+ */
 @SpringBootApplication
-public class PortalApplication {
-
+public class PortalApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(PortalApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PortalApplication.class, args);
 	}
+
 }
--- portal/src/main/java/com/pjh/v1/controller/userController.java
+++ portal/src/main/java/com/pjh/v1/controller/userController.java
@@ -1,5 +1,6 @@
 package com.pjh.v1.controller;
 
+import com.pjh.v1.FileUtil;
 import com.pjh.v1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
