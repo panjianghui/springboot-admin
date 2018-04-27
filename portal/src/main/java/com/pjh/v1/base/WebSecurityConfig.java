827122d2aab8dd123ddf027c1fb80aaafa457edf
panjianghui <546540274@qq.com>
2018-04-27 20:07:10 +0800

    多线程文件下载工具类

--- portal/src/main/java/com/pjh/v1/base/WebSecurityConfig.java
+++ portal/src/main/java/com/pjh/v1/base/WebSecurityConfig.java
@@ -14,14 +14,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().disable(); // 解决spring boot不允许加载frame问题
+//        http.headers().frameOptions().disable(); // 解决spring boot不允许加载frame问题
+        // 设置可以iframe访问
+        http.headers().frameOptions().sameOrigin();
         http
                 .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()//定义不需要登录权限的页面
+                .antMatchers(new String[]{"/js/**","/css/**","/img/**","/images/**","/fonts/**","/**/favicon.ico"}).permitAll()
+                // 任何尚未匹配的URL只需要对用户进行身份验证
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")// 需要登录时指向的页面
+                .defaultSuccessUrl("/admin")
                 .permitAll()
                 .and()
                 .logout()
